e11139c919df88ca368712f9f2337a6bcc1598ea
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2025-05-02 10:00:51 +0200

    Added UI controls

--- app.R
+++ app.R
@@ -1,5 +1,9 @@
 library(shiny)
 
-ui <- fluidPage("Hello, world!")
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
 server <- function(input, output, session) {}
 shinyApp(ui, server)
