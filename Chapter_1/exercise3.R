50baa3458c1651faf985b657cc751a862c6e0ae4
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2025-05-02 10:34:29 +0200

    adds exercise 3

---
+++ Chapter_1/exercise3.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  "then x times 5 is",
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
+  "then x times y is",
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({ 
+    # Access x and y by calling input$ 
+    input$x * input$y
+  })
+}
+
+shinyApp(ui, server)
