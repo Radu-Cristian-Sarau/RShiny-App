7efdb71f4d620a8b90511c3cded89da037b34790
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2025-05-05 22:55:25 +0200

    finishes chapter 2

--- Chapter_2/app.R
+++ Chapter_2/app.R
@@ -15,11 +15,13 @@ ui <- fluidPage(
       sliderInput("num", "Number two", value = 50, min = 0, max = 100),
       sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
       dateInput("dob", "When were you born?"),
-      dateRangeInput("holiday", "When do you want to go on vacation next?")
+      dateRangeInput("holiday", "When do you want to go on vacation next?"),
+      selectInput("state", "What's your favourite state?", state.name),
+      selectInput("state", "What's your favourite state?",
+                  state.name, multiple = TRUE),
     ),
     column(
       3,
-      selectInput("state", "What's your favourite state?", state.name),
       radioButtons("animal", "What's your favourite animal?", animals),
       radioButtons("rb", "Choose your mood:",
                    choiceName = list (
@@ -29,28 +31,45 @@ ui <- fluidPage(
                    ),
                    choiceValues = list("angry", "happy", "sad")
                  ),
-      selectInput("state", "What's your favourite state?",
-                  state.name, multiple = TRUE),
       checkboxGroupInput("animal", "What animals do you like?", animals),
       checkboxInput("cleanup", "Clean up?", value = TRUE),
       checkboxInput("shutdown", "Shut down?", value = TRUE),
       fileInput("upload", NULL),
       actionButton("click", "Click me!"),
-      actionButton("drink", "Drink me!", icon = icon("cocktail"))
-    ),
-    column(
-      3,
+      actionButton("drink", "Drink me!", icon = icon("cocktail")),
       fluidRow(
         actionButton("click", "Click me!", class = "btn-danger"),
         actionButton("drink", "Drink me!", class = "btn-lg btn-success")
       ),
       fluidRow(
         actionButton("eat", "Eat me!", class = "btn-block")
-      )
+      ),
+      textOutput("text"),
+      verbatimTextOutput("code"),
+      textOutput("text2"),
+      verbatimTextOutput("print"),
+    ),
+    column(
+      3,
+      tableOutput("static"),
+      dataTableOutput("dynamic"),
+      plotOutput("plot", width = "400px")
+    ),
+    column(
+      3,
+      
     )
   )
 )
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  output$text <- renderText("Hello, friend!")
+  output$code <- renderPrint(summary(1:10))
+  output$text2 <- renderText("hello!")
+  output$print <- renderPrint("hello!")
+  output$static <- renderTable(head(mtcars))
+  output$dynamic <- renderDataTable(mtcars, options = list(pageLength = 5))
+  output$plot <- renderPlot(plot(1:5), res = 96)
+}
 
 shinyApp(ui, server)
